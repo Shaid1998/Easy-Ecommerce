c9b62e9e0b674bfdfde11e4be381ab8e9213d608
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-09 16:33:09 +0600

    09-07-2023

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Home\HomeSliderController;
 
 
 Route::get('/', function () {
@@ -21,6 +22,14 @@ Route::controller(AdminController::class)->group(function () {
     Route::post('/store/profile', 'StoreProfile')->name('store.profile');
     Route::get('/change/password', 'ChangePassword')->name('change.password');
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
+
+    
+});
+
+
+//Home Slide all route
+Route::controller(HomeSliderController::class)->group(function () {
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
 
     
 });
