5da4b22aa84cd42cacdc3c04b47160c5fa529146
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-09 19:15:37 +0600

    09-07-2023

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::controller(AdminController::class)->group(function () {
 //Home Slide all route
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 
     
 });
